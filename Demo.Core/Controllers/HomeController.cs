9048b58ff2d2aa3329294a635b20c4607ebc3b18
vsha041 <varun.varunsharma@gmail.com>
2018-10-28 08:40:39 +1300

    Added MVC code controller and View for Products page.

---
+++ Demo.Core/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Demo.Core.Models;
+
+namespace Demo.Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            ViewData["Message"] = "Your products page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Products(string productName, 
+            string productNumber, 
+            string productCost, 
+            string productDescription)
+        {
+            ViewData["Message"] = "Your products page.";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Demo.Core/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Core.Models
+{
+    public class ProductModel
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductNumber { get; set; } = string.Empty;
+        public string ProductCost { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+    }
+}
